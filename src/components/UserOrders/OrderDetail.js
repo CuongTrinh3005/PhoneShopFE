c056cb1d6935402e29c828d0148da0d02d7ea4bc
Cuong Trinh <cuongtrinhq@gmail.com>
2021-08-12 21:02:51 +0700

    use orderDetailDTO in user order list

--- src/components/UserOrders/OrderDetail.js
+++ src/components/UserOrders/OrderDetail.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { endpointUser, endpointPublic, getWithAuth, get } from '../HttpUtils';
+import { endpointUser, getWithAuth } from '../HttpUtils';
+import './style.css';
 
 class OrderDetail extends Component {
     state = { orderDetails: [], book: {} }
 
     componentDidMount() {
-        this.fetchOrderDetail(this.props.match.params.orderid).then(() => {
-            for (let detail of this.state.orderDetails)
-                this.prepareForDisplayOrderDetail(detail.orderDetailID.bookId, detail.quantityOrder,
-                    detail.unitPrice, detail.discount)
-        });
+        this.fetchOrderDetail(this.props.match.params.orderid);
     }
 
-    async fetchOrderDetail(id) {
-        await getWithAuth(endpointUser + "/order_details/" + id).then((response) => {
+    fetchOrderDetail(id) {
+        getWithAuth(endpointUser + "/order_details/" + id).then((response) => {
             if (response.status === 200) {
                 this.setState({ orderDetails: response.data })
                 console.log("order data:" + response.data)
@@ -32,71 +29,23 @@ class OrderDetail extends Component {
         let totalPrice = 0;
         for (let index = 0; index < this.state.orderDetails.length; index++) {
             const price = (1 - this.state.orderDetails[index].discount)
-                * this.state.orderDetails[index].unitPrice * this.state.orderDetails[index].quantityOrder;
+                * this.state.orderDetails[index].unitPrice * this.state.orderDetails[index].orderQuantity;
             totalPrice += price;
         }
         return totalPrice;
     }
 
-    prepareForDisplayOrderDetail(id, quantityInp, unitPriceInp, discountInp) {
-        this.fetchBookById(id).then(() => {
-            if (!this.handleCheck(this.state.book)) {
-                this.setState(prevState => {
-                    let book = Object.assign({}, prevState.book);  // creating copy of state variable jasper    
-                    book.quantity = quantityInp;                     // update the name property, assign a new value                 
-                    book.unitPrice = unitPriceInp;
-                    book.discount = discountInp;
-
-                    return { book };                                 // return new object jasper object
-                })
-                this.state.orderDetails.push(this.state.book);
-            }
-            else {
-                this.setState(prevState => ({
-                    orderDetails: prevState.orderDetails.map(
-                        obj => (obj.orderDetailID.bookId === id ? Object.assign(obj,
-                            { quantity: quantityInp }, { unitPrice: unitPriceInp }, { discount: discountInp })
-                            : obj)
-                    )
-                }));
-            }
-
-            this.setState({ orderDetails: this.state.orderDetails })
-        })
-    }
-
-    handleCheck(val) {
-        return this.state.orderDetails.some(item => val.bookId === item.orderDetailID.bookId);
-    }
-
-    async fetchBookById(id) {
-        await get(endpointPublic + "/books/" + id).then((response) => {
-            if (response.status === 200) {
-                this.setState({ book: response.data })
-                // this.setState({ authorIds: response.data.authorIds })
-            }
-        }).catch((error) => console.log("Fetching book by id error: " + error))
-    }
-
-    getBookName(id) {
-        get(endpointPublic + "/books/get-name/" + id).then((response) => {
-            if (response.status === 200) {
-                console.log("Book name: " + response.data)
-                return response.data;
-            }
-        }).catch((error) => console.log("Fetching book by id error: " + error))
-    }
-
     render() {
         return (
             <div>
-                <h3>Order Detail</h3>
-                <h5>Order ID: {this.props.match.params.orderid}</h5>
+                <h3 className="order-title">Order Detail</h3>
+                <h5 className="order-id">Order ID: {this.props.match.params.orderid}</h5>
                 <table>
                     <thead>
                         <tr>
                             <th>Book Id</th>
                             <th>Book Name</th>
+                            <th>Image</th>
                             <th>Quantity</th>
                             <th>Discount</th>
                             <th>Unit Price</th>
@@ -105,13 +54,19 @@ class OrderDetail extends Component {
                     </thead>
                     <tbody>
                         {this.state.orderDetails.map((detail) => (
-                            <tr key={detail.orderDetailID.bookId}>
-                                <td>{detail.orderDetailID.bookId}</td>
+                            <tr key={detail.bookId}>
+                                <td>{detail.bookId}</td>
                                 <td>{detail.bookName}</td>
-                                <td>{detail.quantityOrder}</td>
+                                <td>
+                                    <img src={`data:image/jpeg;base64,${detail.photo}`}
+                                        alt="Image loading..."
+                                        width="150" height="100">
+                                    </img>
+                                </td>
+                                <td>{detail.orderQuantity}</td>
                                 <td>{detail.discount * 100}%</td>
                                 <td>{this.formatter.format(detail.unitPrice)}</td>
-                                <td>{this.formatter.format((1 - detail.discount) * detail.unitPrice * detail.quantityOrder)}</td>
+                                <td>{this.formatter.format((1 - detail.discount) * detail.unitPrice * detail.orderQuantity)}</td>
                             </tr>
                         ))}
                     </tbody>
